2e0673599d3922a5d89bb559a77c3e09bf3e13a6
Walter Dalla <WalterDallaTorreNeto@gmail.com>
2023-09-23 02:21:18 -0300

    add parent machine block
    Signed-off-by: Walter Dalla <WalterDallaTorreNeto@gmail.com>

--- src/main/java/com/goodoldtimes/ic2/block/Custom/MaceratorBlock.java
+++ src/main/java/com/goodoldtimes/ic2/block/Custom/MaceratorBlock.java
@@ -23,60 +23,12 @@ import net.minecraft.util.hit.BlockHitResult;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class MaceratorBlock extends BlockWithEntity implements BlockEntityProvider {
+public class MaceratorBlock extends ParentMachineBlock  {
     public static final String BLOCK_ID = "macerator_block";
 
     public MaceratorBlock(Settings settings) {
-        super(settings);
-    }
-
-    @Override
-    public void onPlaced(World world, BlockPos pos, BlockState state, @Nullable LivingEntity placer, ItemStack itemStack) {
-        super.onPlaced(world, pos, state, placer, itemStack);
-    }
-
-    /* BLOCK ENTITY */
-
-    @Override
-    public void onStateReplaced(BlockState state, World world, BlockPos pos, BlockState newState, boolean moved) {
-        state = world.getBlockState(pos);
-        if (state.getBlock() != newState.getBlock()) {
-            BlockEntity blockEntity = world.getBlockEntity(pos);
-            if (blockEntity instanceof MaceratorBlockEntity) {
-                ItemScatterer.spawn(world, pos, (MaceratorBlockEntity)blockEntity);
-                world.updateComparators(pos,this);
-            }
-            super.onStateReplaced(state, world, pos, newState, moved);
-        }
+        super(settings, BLOCK_ID);
     }
 
 
-
-    @Override
-    public ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit) {
-        if (!world.isClient) {
-            NamedScreenHandlerFactory screenHandlerFactory = ((MaceratorBlockEntity) world.getBlockEntity(pos));
-
-            if (screenHandlerFactory != null) {
-                player.openHandledScreen(screenHandlerFactory);
-            }
-        }
-
-        return ActionResult.SUCCESS;
-    }
-
-    @Override
-    public BlockEntity createBlockEntity(BlockPos pos, BlockState state) {
-        return new MaceratorBlockEntity(pos, state);
-    }
-    @Override
-    public BlockRenderType getRenderType(BlockState state) {
-        return BlockRenderType.MODEL;
-    }
-
-    @Nullable
-    @Override
-    public <T extends BlockEntity> BlockEntityTicker<T> getTicker(World world, BlockState state, BlockEntityType<T> type) {
-        return checkType(type, ModBlockEntities.MACERATOR, MaceratorBlockEntity::tick);
-    }
 }
---
+++ src/main/java/com/goodoldtimes/ic2/block/Custom/ParentMachineBlock.java
@@ -0,0 +1,82 @@
+package com.goodoldtimes.ic2.block.Custom;
+
+import com.goodoldtimes.Block.Entity.ModBlockEntities;
+import com.goodoldtimes.ic2.block.entity.MaceratorBlockEntity;
+import net.minecraft.block.BlockEntityProvider;
+import net.minecraft.block.BlockRenderType;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.BlockWithEntity;
+import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.block.entity.BlockEntityTicker;
+import net.minecraft.block.entity.BlockEntityType;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.screen.NamedScreenHandlerFactory;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import net.minecraft.util.ItemScatterer;
+import net.minecraft.util.hit.BlockHitResult;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import org.jetbrains.annotations.Nullable;
+
+public class ParentMachineBlock extends BlockWithEntity implements BlockEntityProvider {
+
+    public static String BLOCK_ID = "parent_machine_block";
+
+    public ParentMachineBlock(Settings settings, String blockId) {
+        super(settings);
+        BLOCK_ID = blockId;
+    }
+
+    @Override
+    public void onPlaced(World world, BlockPos pos, BlockState state, @Nullable LivingEntity placer, ItemStack itemStack) {
+        super.onPlaced(world, pos, state, placer, itemStack);
+    }
+
+    /* BLOCK ENTITY */
+
+    @Override
+    public void onStateReplaced(BlockState state, World world, BlockPos pos, BlockState newState, boolean moved) {
+        state = world.getBlockState(pos);
+        if (state.getBlock() != newState.getBlock()) {
+            BlockEntity blockEntity = world.getBlockEntity(pos);
+            if (blockEntity instanceof MaceratorBlockEntity) {
+                ItemScatterer.spawn(world, pos, (MaceratorBlockEntity)blockEntity);
+                world.updateComparators(pos,this);
+            }
+            super.onStateReplaced(state, world, pos, newState, moved);
+        }
+    }
+
+
+
+    @Override
+    public ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit) {
+        if (!world.isClient) {
+            NamedScreenHandlerFactory screenHandlerFactory = ((MaceratorBlockEntity) world.getBlockEntity(pos));
+
+            if (screenHandlerFactory != null) {
+                player.openHandledScreen(screenHandlerFactory);
+            }
+        }
+
+        return ActionResult.SUCCESS;
+    }
+
+    @Override
+    public BlockEntity createBlockEntity(BlockPos pos, BlockState state) {
+        return new MaceratorBlockEntity(pos, state);
+    }
+    @Override
+    public BlockRenderType getRenderType(BlockState state) {
+        return BlockRenderType.MODEL;
+    }
+
+    @Nullable
+    @Override
+    public <T extends BlockEntity> BlockEntityTicker<T> getTicker(World world, BlockState state, BlockEntityType<T> type) {
+        return checkType(type, ModBlockEntities.MACERATOR, MaceratorBlockEntity::tick);
+    }
+}
