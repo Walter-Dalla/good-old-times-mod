70ab2b39574905c2050a314b4533874d2780c28c
Walter Dalla <WalterDallaTorreNeto@gmail.com>
2023-09-25 00:47:08 -0300

    add Screen helper
    Signed-off-by: Walter Dalla <WalterDallaTorreNeto@gmail.com>

--- src/client/java/com/goodoldtimes/ic2/Screen/MaceratorScreen.java
+++ src/client/java/com/goodoldtimes/ic2/Screen/MaceratorScreen.java
@@ -23,18 +23,9 @@ public class MaceratorScreen extends HandledScreen<MaceratorScreenHandler> {
 
     @Override
     protected void drawBackground(DrawContext context, float delta, int mouseX, int mouseY) {
-        RenderSystem.setShader(GameRenderer::getPositionTexProgram);
-        RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, 1.0F);
-        //RenderSystem.setShaderTexture(0, TEXTURE);
-        int x = (width - backgroundWidth) / 2;
-        int y = (height - backgroundHeight) / 2;
-
-        MatrixStack matrices = context.getMatrices();
-        context.drawTexture(TEXTURE, x, y, 0, 0, backgroundWidth, backgroundHeight);
-
-        context.drawTexture(TEXTURE, x + 56, y + 36, 176, 0, 14, handler.getScaledEnergy());
-        context.drawTexture(TEXTURE, x + 79, y + 34, 176, 14,  handler.getScaledProgress(),14);
+        ScreenHelper.DrawBackgroundGeneric(context, width, backgroundWidth, height, backgroundHeight, TEXTURE, handler);
     }
+
 
 
     @Override
@@ -47,7 +38,6 @@ public class MaceratorScreen extends HandledScreen<MaceratorScreenHandler> {
     @Override
     protected void init() {
         super.init();
-        // Center the title
-        titleX = (backgroundWidth - textRenderer.getWidth(title)) / 2;
+        titleX = ScreenHelper.AddCenterTitle(backgroundWidth, textRenderer, title);
     }
 }
